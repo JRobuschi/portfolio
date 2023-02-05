a5b986fac913d40a879b1ad0b843e5dfddc35b11
JRobuschi <juanrobuschi5@gmail.com>
2023-02-05 19:39:54 -0300

    Update general app

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import './about.css'
-import ME from '../../assets/me-about.jpg'
-import { FaAward } from 'react-icons/fa'
-import { FiUsers } from 'react-icons/fi'
-import { VscFolderLibrary } from 'react-icons/vsc'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
@@ -20,35 +20,40 @@ const About = () => {
 
         <div className="about__content">
           <div className="about__cards">
-
             <article className="about__card">
-              <FaAward className='about__icon' />
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>1+ Years Proyects</small>
             </article>
 
             <article className="about__card">
-              <FiUsers className='about__icon' />
+              <FiUsers className="about__icon" />
               <h5>Clients</h5>
               <small>Professors</small>
             </article>
 
             <article className="about__card">
-              <VscFolderLibrary className='about__icon' />
+              <VscFolderLibrary className="about__icon" />
               <h5>Proyects</h5>
-              <small>2+ Completed</small>
+              <small>3+ Completed</small>
             </article>
           </div>
 
           <p>
-            I'm a Fullstack Developer, I studied in the Digital House platform, and finished with success the integrator project, completing all course requirements, in which we had to carry out an e-commerce of computer elements, in groups armed by the teachers.
+            I'am a Fullstack Developer, I studied on the Digital House platform,
+            and I successfully completed the integration project, completing all
+            the course requirements, in which we had to carry out an e-commerce
+            of computer elements, in groups made up of teachers. and also
+            finished a DevPlace bootcamp where I ended up with a group project.
           </p>
 
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,62 +1,72 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
 
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG6 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG6 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
 
 const data = [
   {
     id: 1,
     image: IMG4,
-    title: 'Email notification',
-    github: 'https://github.com/JRobuschi?tab=repositories',
-    demo: 'https://portfolio-jrobuschi.vercel.app/#contact'
+    title: "Email notification",
+    github: "https://github.com/JRobuschi?tab=repositories",
+    demo: "https://portfolio-jrobuschi.vercel.app/#contact",
   },
   {
     id: 2,
     image: IMG6,
-    title: 'Hospital App',
-    github: 'https://github.com/JRobuschi/hospitalApp',
-    demo: 'https://euphonious-pothos-4520ad.netlify.app/'
+    title: "Hospital App",
+    github: "https://github.com/JRobuschi/hospitalApp",
+    demo: "https://euphonious-pothos-4520ad.netlify.app/",
   },
   {
     id: 3,
     image: IMG2,
-    title: 'Crypto currencey Dashboard & Financial Visualization "Coming soon"',
-    github: 'https://github.com/JRobuschi?tab=repositories',
-    demo: 'https://portfolio-jrobuschi.vercel.app/'
+    title: "Crypto currencey Dashboard & Financial Visualization ",
+    github: "https://github.com/JRobuschi/devCoin-front",
+    demo: "https://dev-coin.web.app/",
   },
-
-]
+];
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className='container portfolio__container'>
-        {
-          data.map(({ id, image, title, github, demo }) => {
-            return (
-              <article key={id} className='portfolio__item'>
-                <div className='portfolio__item-image'>
-                  <img src={image} alt={title} />
-                </div>
-                <h3>{title}</h3>
-                <div className="portfolio__item-cta">
-                  <a href={github} className='btn' target='_blank' rel="noreferrer">Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
-                </div>
-              </article>
-            )
-          })
-        }
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
